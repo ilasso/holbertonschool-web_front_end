a32909590e07846c05cd35cd79f7d556c4508fb8
ilasso <1204@holbertonschool.com>
2020-09-29 09:26:12 -0500

    doc functions

--- 0x0C-Javascript_advanced/0-welcome.js
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -1,3 +1,11 @@
+/**
+ *welcome - Funtion that alerts 2 strings and "!"
+ *firstName: firts string
+ *lastName: last string
+ *Return: none, only alerts
+ *On Error: no error controls
+ */
+
 function welcome (firstName, lastName) {
   const fullName = firstName + ' ' + lastName;
   function displayFullName () {
--- 0x0C-Javascript_advanced/1-nested_functions.js
+++ 0x0C-Javascript_advanced/1-nested_functions.js
@@ -1,8 +1,26 @@
+/**
+ *outer - Funtion that invoke a inner function and alert globalVariable
+ *Return: none, only alert and call inner function
+ *On Error: no error controls
+ */
+
 let globalVariable = "Welcome";
 function outer() {
   let course = "Holberton";
+  /**
+   *inner - Funtion that invoke a inception function and alert globalVariable
+            concatenates with course var
+   *Return: none, only alert and call inception function
+   *On Error: no error controls
+   */
   function inner() {
     let exclamation = "!";
+    /**
+     *inception - Funtion that alert globalVariable
+              concatenates with course var and exclamation var
+     *Return: none, only alert
+     *On Error: no error controls
+     */
     function inception() {
       alert(globalVariable + " " + course + " " + exclamation);
     }
